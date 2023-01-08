a2fd81768bd5c95f52783dfea666d08a37b5eca3
dan <dan@pop-os.localdomain>
2023-01-08 15:42:52 -0600

    cloner.sh

--- getrepo.py
+++ getrepo.py
@@ -1,5 +1,5 @@
 
-import json, sys
+import json, sys, subprocess, time, os
 from urllib.request import urlopen
 
 
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     if (len(sys.argv) == 1):
         print("Usage: gitget <username>")
         sys.exit(1)
+    
+    user = sys.argv[1] ## github username
 
-    user = sys.argv[1] ## github username 
     repoURL = ('https://api.github.com/users/' + user) + '/repos'
     html = urlopen(repoURL).read().decode('utf-8')
     repoJSON = json.loads(html)
@@ -16,4 +17,7 @@ if __name__ == "__main__":
     for url in repoJSON:
        file.write("{}".format(url["html_url"] + ".git" + "\n"))
 
+
+    ##os.system('bash cloner.sh ' + user + '.txt')
+    
     sys.exit(1)
